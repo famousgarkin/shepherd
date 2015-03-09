4f90416ff05f45446fc3f80838aa474336ef3dc3
Ján Dzurek <famousgarkin@outlook.com>
2015-03-09 20:13:51 +0100

    items/item routing, nav stub

--- app.js
+++ app.js
@@ -4,5 +4,41 @@ App = Ember.Application.create({
 })
 
 App.Router.map(function() {
-    this.resource('item', {path: '/:path'})
+    this.resource('items', {path: '/'}, function() {
+        this.resource('item', {path: '/*path'})
+    })
+})
+
+App.IndexRoute = Ember.Route.extend({
+    beforeModel: function() {
+        this.transitionTo('items')
+    },
+})
+
+App.ItemsIndexRoute = Ember.Route.extend({
+    beforeModel: function() {
+        this.transitionTo('item', {path: ''})
+    }
+})
+
+App.ItemsRoute = Ember.Route.extend({
+    model: function() {
+        // TODO: get navigation from configuration
+        return {
+            navigation: [
+                {name: 'page1/test', url: 'url1'},
+                {name: 'page2/test', url: 'url2'},
+            ],
+        }
+    },
+})
+
+App.ItemRoute = Ember.Route.extend({
+    model: function(params) {
+        var model = this.modelFor('items')
+        var item = model.navigation.find(function(item) {
+            return item.name == params.path
+        })
+        return item
+    },
 })
