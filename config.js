5b7c6acfc1604ac351636cd5d2c9c33083092a1f
Ján Dzurek <famousgarkin@outlook.com>
2015-03-10 09:00:36 +0100

    nav partial

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ var config = {}
 config.title = 'My Shepherd'
 
 config.items = [
+    {name: 'index', url: ''},
     {name: 'page    1', url: 'url1'},
     {name: 'page  2??', url: 'url2'},
     {name: 'page 3///test', url: 'url3'},
